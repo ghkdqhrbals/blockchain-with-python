d9094b52defac5905cd3283f4d5bdb7c3d7e67d4
gyuminhwangbo <ghkdqhrbals@naver.com>
2021-06-22 15:33:01 +0900

    Tx, TxPool, Node(+Bootstrap node), Network, Server(prototype 메세지 서버) 모든 structure및 func

--- node.py
+++ node.py
@@ -31,25 +31,25 @@ class Network:
         return self.peerlist[node.ID]
 class Node:
     # KEY 생성 및 노드 생성.
-    def __init__(self,nodeID,network,Energy,Money,state):
+    def __init__(self,number,network,Energy,Money,state):
         #print("현재 네트워크 피어 개수 : ",network.getSize())
         self.state = state # user : 0, minor : 1
         self.Energy = Energy
         self.Money = Money
         self.peerlistm = network.getPeerlist()
-        self.nodeID = nodeID
+        self.number = number
         self.isMinor = False
         self.pub_key = ""
         self.pub_key_str = ""
         self.ID = ""
         pkey = crypto.PKey()
-        pkey.generate_key(crypto.TYPE_RSA, 1024+nodeID)
+        pkey.generate_key(crypto.TYPE_RSA, 1024+number)
         print("RSA KEY 생성완료")
-        with open("public"+str(nodeID)+".pem", 'ab+') as f:
+        with open("public"+str(number)+".pem", 'ab+') as f:
             f.write(crypto.dump_publickey(crypto.FILETYPE_PEM, pkey))
-        with open("private"+str(nodeID)+".pem", 'ab+') as f:
+        with open("private"+str(number)+".pem", 'ab+') as f:
             f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
-        with open("public"+str(self.nodeID)+".pem", 'rb+') as f:
+        with open("public"+str(self.number)+".pem", 'rb+') as f:
             for i in f.readlines():
                 j = str(i)
                 if '---' in j:
@@ -62,19 +62,20 @@ class Node:
             self.ID = hashlib.sha256(self.pub_key_str.encode('utf-8')).hexdigest()[:16]
             print("ID : "+self.ID)
 
-        with open("public" + str(self.nodeID) + ".pem", 'rb+') as f:
+        with open("public" + str(self.number) + ".pem", 'rb+') as f:
             self.pub_key = crypto.load_publickey(crypto.FILETYPE_PEM, f.read())
 
             #print(crypto.b16encode(bytes(str(self.pub_key),encoding='utf-8')).decode())
             #print(bytes(str(self.pub_key),encoding='utf-8'))
             #self.pub_key = crypto.load_publickey(crypto.FILETYPE_PEM, f.read())
-
+    def getID(self):
+        return self.ID
     def getPubKey(self):
         return self.pub_key
     def sendTx(self,To,Energy,Money,GasPrice,sig1,sig2):
-        with open("private"+str(self.nodeID)+".pem", 'rb+') as f:
+        with open("private"+str(self.number)+".pem", 'rb+') as f:
             self.priv_key = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
-        with open("public"+str(self.nodeID)+".pem", 'rb+') as f:
+        with open("public"+str(self.number)+".pem", 'rb+') as f:
             self.pub_key = crypto.load_publickey(crypto.FILETYPE_PEM, f.read())
 
 
@@ -102,16 +103,16 @@ class MSG():
     def getMSGprint(self,node1):
         print("-----getMSG start-------")
         for i in self.dicts:
-            if str(node1.nodeID) == str(i[1]):
+            if str(node1.ID) == str(i[1]):
                 # 밑에 수정 필요
                 print(str(node1)+" MSG : "+str(i[2]) + " FROM : "+ str(i[0]))
         print("-----getMSG end-------")
     def sendMSGprint(self, node1):
         print("-----sendMSG start-------")
         for i in self.dicts:
-            if str(node1.nodeID) == str(i[0]):
+            if str(node1.ID) == str(i[0]):
                 # 밑에 수정 필요
-                print(str(node1) + " MSG : " + str(i[2]) + " To : " + str(i[1]))
+                print(str(node1.ID) + " MSG : " + str(i[2]) + " To : " + str(i[1]))
         print("-----getMSG end-------")
 
 
@@ -134,7 +135,7 @@ if __name__ == '__main__':
     print(bootstrap.getPeerInfo(nodeA)) # 네트워크에서 nodeA정보 반환.
 
     print("nodeA -> nodeB로 Tx 전송")
-    TxPool.addTx(nodeA.sendTx(nodeB.getPubKey(), 50, 4.1, 0.04, 0, 0))
+    TxPool.addTx(nodeA.sendTx(nodeB.getPubKey(), 50, 4.1, 0.04, 0, 0)) # TxPool에 Tx추가.
 
     msgServer.getMSGprint(nodeB) # nodeB가 받은 message 출력
     msgServer.sendMSGprint(nodeB) # nodeB가 전송한 message 출력
